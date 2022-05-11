d923635f3b097b183fb63c530c18cc603bc6663d
hihihaha321 <102670833+hihihaha321@users.noreply.github.com>
2022-05-11 23:03:19 +0700

    Add files via upload

---
+++ frontend/build/precache-manifest.ac2065940611e0eee20d5efb3c619275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc0ecfd2578fc84fd3910709e9aa927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "955b997ca6e8734e196b",
+    "url": "/static/css/main.a370e806.chunk.css"
+  },
+  {
+    "revision": "74f77146fc36a4722793",
+    "url": "/static/js/2.2ffe1190.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2ffe1190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "955b997ca6e8734e196b",
+    "url": "/static/js/main.cbcd0992.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
